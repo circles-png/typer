c3f027fe8cd2d1297518d60de26b229ff1e96a41
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-08-17 15:58:18 +1000

    add a text area, status bar, placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-const App = () => <>
-  <div className='grid place-content-center h-full'>
-    and now i have a website
-  </div>
-</>
+import { useState } from 'react'
+
+const App = () => {
+  const placeholders = [
+    'Type',
+    'Dear diary'
+  ],
+        [ wordCount, setWordCount ] = useState(0)
+  return <>
+    <div className='grid place-content-center h-full'>
+      <textarea
+        className='resize-none appearance-none p-16 w-screen h-screen'
+        placeholder={`${placeholders[Math.floor(Math.random() * placeholders.length)]}...`}
+        onInput={event => {
+          setWordCount(Array.from(event.currentTarget.value.matchAll(/\S+\s?/ug)).length)
+        }}
+      />
+      <span className='absolute bottom-4 right-4 flex divide-x-2 [&_*]:px-2 [&_*:last-child]:pr-0'>
+        <button>save</button>
+        <span>words: {wordCount}</span>
+      </span>
+    </div>
+  </>
+}
 
 export default App
